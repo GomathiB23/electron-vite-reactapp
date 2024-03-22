cdc19eafdd27f7fe633de7699a7e86eb47e746c1
GomathiB23 <gomathibalakrishnan9@gmail.com>
2024-03-22 13:55:20 +0530

    third commit

--- electron/electron-env.d.js
+++ electron/electron-env.d.js
@@ -1,27 +1,49 @@
+// /// <reference types="vite-plugin-electron/electron-env" />
+
+// declare namespace NodeJS {
+//   interface ProcessEnv {
+//     /**
+//      * The built directory structure
+//      *
+//      * ```tree
+//      * ├─┬─┬ dist
+//      * │ │ └── index.html
+//      * │ │
+//      * │ ├─┬ dist-electron
+//      * │ │ ├── main.js
+//      * │ │ └── preload.js
+//      * │
+//      * ```
+//      */
+//     DIST: string
+//     /** /dist/ or /public/ */
+//     VITE_PUBLIC: string
+//   }
+// }
+
+// // Used in Renderer process, expose in `preload.ts`
+// interface Window {
+//   ipcRenderer: import('electron').IpcRenderer
+// }
+
+
+
 /// <reference types="vite-plugin-electron/electron-env" />
+import { ipcRenderer } from 'electron';
 
-declare namespace NodeJS {
-  interface ProcessEnv {
-    /**
-     * The built directory structure
-     *
-     * ```tree
-     * ├─┬─┬ dist
-     * │ │ └── index.html
-     * │ │
-     * │ ├─┬ dist-electron
-     * │ │ ├── main.js
-     * │ │ └── preload.js
-     * │
-     * ```
-     */
-    DIST: string
-    /** /dist/ or /public/ */
-    VITE_PUBLIC: string
-  }
-}
+/**
+ * @typedef {Object} ProcessEnv
+ * @property {string} DIST
+ * @property {string} VITE_PUBLIC
+ */
 
-// Used in Renderer process, expose in `preload.ts`
-interface Window {
-  ipcRenderer: import('electron').IpcRenderer
-}
+/** @namespace NodeJS */
+/** @property {ProcessEnv} ProcessEnv */
+
+
+
+
+const ipcRenderer = require('electron').ipcRenderer;
+
+
+export {};
